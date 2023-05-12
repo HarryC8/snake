71a010047d044806f3408debf4be4791c90891ff
HarryC8 <127170776+HarryC8@users.noreply.github.com>
2023-05-12 16:42:56 -0500

    Update pxt.json, main.py

---
+++ main.py
@@ -0,0 +1,101 @@
+import pygame
+import random
+
+# initialize Pygame
+pygame.init()
+
+# set up the game window
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Snake Game")
+
+# set up the colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# set up the game variables
+BLOCK_SIZE = 10
+FPS = 30
+font = pygame.font.SysFont(None, 25)
+
+# define the Snake class
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.positions = [((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+        self.color = GREEN
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def turn(self, point):
+        if self.length > 1 and (point[0] * -1, point[1] * -1) == self.direction:
+            return
+        else:
+            self.direction = point
+
+    def move(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0] + (x * BLOCK_SIZE)) % WINDOW_WIDTH), (cur[1] + (y * BLOCK_SIZE)) % WINDOW_HEIGHT)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+    def reset(self):
+        self.length = 1
+        self.positions = [((WINDOW_WIDTH / 2), (WINDOW_HEIGHT / 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+
+    def draw(self, surface):
+        for p in self.positions:
+            r = pygame.Rect((p[0], p[1]), (BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(surface, self.color, r)
+            pygame.draw.rect(surface, BLACK, r, 1)
+
+    def handle_keys(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.turn(UP)
+                elif event.key == pygame.K_DOWN:
+                    self.turn(DOWN)
+                elif event.key == pygame.K_LEFT:
+                    self.turn(LEFT)
+                elif event.key == pygame.K_RIGHT:
+                    self.turn(RIGHT)
+
+# define the Food class
+class Food:
+    def __init__(self):
+        self.position = (0, 0)
+        self.color = RED
+        self.randomize_position()
+
+    def randomize_position(self):
+        self.position = (random.randint(0, (WINDOW_WIDTH // BLOCK_SIZE)) * BLOCK_SIZE,
+                          random.randint(0, (WINDOW_HEIGHT // BLOCK_SIZE)) * BLOCK_SIZE)
+
+    def draw(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(surface, self.color, r)
+        pygame.draw.rect(surface, BLACK, r, 1)
+
+# set up the directions
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# define the
+
